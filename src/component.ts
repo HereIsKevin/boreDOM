b49226e909ea409123a6edd51daafc6e97cd9892
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-06-15 12:28:21 -0700

    Improve component.ts

--- src/component.ts
+++ src/component.ts
@@ -1,29 +1,49 @@
-export { IConstructable, defineElement, exportHandler, Component };
+export { boundMethod, customElement, Component };
 
 import { html, render } from "./dom";
+
+type Dictionary<T> = { [key: string]: T };
+type Method = (...args: any[]) => any;
+type EventHandler = (event: Event) => void;
+type Primitive = number | string | boolean;
+type ComponentDecorator = <T extends Component>(
+  componentClass: IConstructable<T>
+) => void;
+type ChangeHandler = (newValue: string, oldValue: string) => void;
 
 interface IConstructable<T> {
   new (...parameters: any[]): T;
 }
 
 interface IWindow extends Window {
-  BoreHandlers?: { [key: string]: EventHandler };
-  BoreHandlersIndex?: number;
+  eventHandlers?: Dictionary<EventHandler>;
+  eventHandlersIndex?: number;
 }
 
 declare const window: IWindow;
 
-type ComponentWrapper = <T extends Component>(
-  componentClass: IConstructable<T>
-) => void;
+function boundMethod(
+  target: object,
+  key: string,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor {
+  let method: Method = descriptor.value;
 
-type ChangeHandler = (newValue: string, oldValue: string) => void;
-type EventHandler = (event: Event) => void;
+  return {
+    configurable: true,
+    get(): Method {
+      return method.bind(this);
+    },
+    set(value: Method): void {
+      method = value;
+    },
+  };
+}
 
-function defineElement<T extends Component>(
+function customElement<T extends Component>(
   name: string,
   component?: IConstructable<T>
-): ComponentWrapper | undefined {
+): ComponentDecorator | undefined {
   if (typeof component !== "undefined") {
     window.customElements.define(name, component);
   } else {
@@ -32,58 +52,44 @@ function defineElement<T extends Component>(
   }
 }
 
-function exportHandler(handler: EventHandler): () => string {
-  if (typeof window.BoreHandlers === "undefined") {
-    window.BoreHandlers = {};
+function eventHandler(handler: EventHandler): string {
+  if (typeof window.eventHandlers === "undefined") {
+    window.eventHandlers = {};
   }
 
-  if (typeof window.BoreHandlersIndex === "undefined") {
-    window.BoreHandlersIndex = 0;
+  if (typeof window.eventHandlersIndex === "undefined") {
+    window.eventHandlersIndex = 0;
   } else {
-    window.BoreHandlersIndex++;
+    window.eventHandlersIndex++;
   }
 
-  const index = window.BoreHandlersIndex;
-  window.BoreHandlers[`handler${index}`] = handler;
+  const index = window.eventHandlersIndex;
+  window.eventHandlers[`handler${index}`] = handler;
 
-  return () => `BoreHandlers.handler${index}(event);`;
+  return `window.eventHandlers.handler${index}(event);`;
+}
+
+function isPrimitive(value: any): value is Primitive {
+  const type: string = typeof value;
+  return type === "string" || type === "number" || type === "boolean";
 }
 
 class Component extends HTMLElement {
   protected static readonly observedAttributes: string[] = [];
 
   protected shadow: ShadowRoot;
-  protected changeHandlers: { [key: string]: ChangeHandler };
+  protected changeHandlers: Dictionary<ChangeHandler>;
 
   public constructor() {
     super();
 
-    this.changeHandlers = {};
     this.shadow = this.attachShadow({ mode: "open" });
-  }
-
-  protected exportHandler(handler: EventHandler): () => string {
-    if (typeof window.BoreHandlers === "undefined") {
-      window.BoreHandlers = {};
-    }
-
-    if (typeof window.BoreHandlersIndex === "undefined") {
-      window.BoreHandlersIndex = 0;
-    } else {
-      window.BoreHandlersIndex++;
-    }
-
-    handler = handler.bind(this);
-
-    const index = window.BoreHandlersIndex;
-    window.BoreHandlers[`handler${index}`] = handler;
-
-    return () => `BoreHandlers.handler${index}(event);`;
+    this.changeHandlers = {};
   }
 
   private connectedCallback(): void {
     this.connected();
-    this.paint();
+    this.update();
   }
 
   private disconnectedCallback(): void {
@@ -95,18 +101,14 @@ class Component extends HTMLElement {
     oldValue: string,
     newValue: string
   ): void {
-    for (const [key, callback] of Object.entries(this.changeHandlers)) {
-      if (key === name) {
-        callback(oldValue, newValue);
-      }
+    const handler: ChangeHandler | undefined = this.changeHandlers[name];
+
+    if (typeof handler !== "undefined") {
+      handler(oldValue, newValue);
     }
 
     this.changed(name, oldValue, newValue);
-    this.paint();
-  }
-
-  private paint(): void {
-    render(this.shadow, this.render());
+    this.update();
   }
 
   protected connected(): void {}
@@ -117,30 +119,50 @@ class Component extends HTMLElement {
 
   protected html(
     strings: TemplateStringsArray,
-    ...items: string[]
+    ...values: (EventHandler | Primitive | Primitive[])[]
   ): DocumentFragment {
-    let contents: string[] = [strings[0]];
+    let output: string[] = [strings[0]];
 
     for (let [index, item] of strings.slice(1, strings.length).entries()) {
-      contents.push(items[index]);
-      contents.push(item);
+      const code: EventHandler | Primitive | Primitive[] = values[index];
+
+      if (isPrimitive(code)) {
+        output.push(String(code));
+      } else if (Array.isArray(code)) {
+        output.push(code.join(""));
+      } else {
+        output.push(`"${eventHandler(code)}"`);
+      }
+
+      output.push(item);
     }
 
-    return html(contents.join(""));
+    return html(output.join(""));
   }
 
-  public get properties(): string[] {
-    return new Proxy([], {
-      get: (target: string[], name: string): string =>
-        this.getAttribute(name) || "",
-      set: (target: string[], name: string, value: string): boolean => {
-        this.setAttribute(name, value);
-        return true;
-      },
-    });
+  protected update(): void {
+    render(this.shadow, this.render());
   }
 
   protected render(): DocumentFragment {
-    return this.html``;
+    return this.html`<p>Hello, world!</p>`;
+  }
+
+  public get properties(): Dictionary<string> {
+    return new Proxy(
+      {},
+      {
+        get: (target: Dictionary<string>, name: string): string =>
+          this.getAttribute(name) || "",
+        set: (
+          target: Dictionary<string>,
+          name: string,
+          value: string
+        ): boolean => {
+          this.setAttribute(name, value);
+          return true;
+        },
+      }
+    );
   }
 }
