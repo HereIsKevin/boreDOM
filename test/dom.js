efde38119dd7b544a6512dc60419fbc91ff7f43a
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-06 14:29:18 -0700

    Finish on dom test cases

--- test/dom.js
+++ test/dom.js
@@ -1,4 +1,4 @@
-import { html } from "../build/dom.js";
+import { html, render } from "../build/dom.js";
 
 describe("dom", function () {
   describe("html", function () {
@@ -19,6 +19,39 @@ describe("dom", function () {
 
     it("creates a document fragment", function () {
       assert.instanceOf(html("<div></div>"), DocumentFragment);
+    });
+  });
+
+  describe("render", function () {
+    it("updates to empty nodes properly", function () {
+      const documentFragment = html("");
+      render(documentFragment, html("<div></div>"));
+
+      assert.deepEqual(documentFragment, html("<div></div>"));
+    });
+
+    it("updates from empty nodes properly", function () {
+      const documentFragment = html("<div></div>");
+      render(documentFragment, html(""));
+
+      assert.deepEqual(documentFragment, html(""));
+    });
+
+    it("updates different nodes properly", function () {
+      const documentFragment = html(
+        '<div><div id="empty"></div></div><div class="blah">Some Content</div>'
+      );
+      render(documentFragment, html('<div id="stuff">Things</div>'));
+
+      assert.deepEqual(documentFragment, html('<div id="stuff">Things</div>'));
+    });
+
+    it("moves nodes instead of copying", function () {
+      const documentFragment = html("<div><div></div></div>");
+      const updated = html("<div></div><div></div>");
+      render(documentFragment, updated);
+
+      assert.deepEqual(updated, html("<div></div>"));
     });
   });
 });
