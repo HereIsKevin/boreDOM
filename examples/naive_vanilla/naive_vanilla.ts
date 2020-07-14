0b01999aa7564470d0e98c013e3805f3ed0a9345
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-14 11:50:34 -0700

    Implement naive vanillajs example for comparison

---
+++ examples/naive_vanilla/naive_vanilla.ts
@@ -0,0 +1,53 @@
+function html(
+  strings: TemplateStringsArray,
+  ...values: string[]
+): DocumentFragment {
+  const result: string[] = [strings[0]];
+
+  for (const [index, value] of strings.slice(1).entries()) {
+    result.push(values[index]);
+    result.push(value);
+  }
+
+  return document.createRange().createContextualFragment(result.join(""));
+}
+
+function render(node: Node, template: DocumentFragment): void {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+
+  node.appendChild(template);
+}
+
+function template(value: number) {
+  return html`<div id="display">Value is currently: ${String(value)}</div>
+    <button id="increment">Click to increment</button>
+    ${[...Array(value).keys()].map((x) => `<div>${x}</div>`).join("")}`;
+}
+
+function update(node: Element, value: number) {
+  console.time("render");
+  render(node, template(value));
+  console.timeEnd("render");
+
+  document.getElementById("increment")?.addEventListener("click", () => {
+    value++;
+
+    if (target === null) {
+      throw new Error("target missing");
+    }
+
+    update(target, value);
+  });
+}
+
+let value = 0;
+
+const target = document.getElementById("app");
+
+if (target === null) {
+  throw new Error("target missing");
+}
+
+update(target, value);
