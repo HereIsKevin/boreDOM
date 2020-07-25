71cac818a6e29d6a15d7d875bec36f4166c197c6
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-25 16:09:56 -0700

    Create an old DOM example

---
+++ examples/dom/dom.ts
@@ -0,0 +1,30 @@
+import { dom } from "../../dist/esm/index.js";
+
+const { html, render } = dom;
+
+function template(value: number) {
+  return html(`<div id="display">Value is currently: ${String(value)}</div>
+    <button id="increment">Click to increment</button>
+    ${[...Array(value).keys()].map((x) => `<div>${x}</div>`).join("")}`);
+}
+
+function update(node: Element, value: number) {
+  console.time("render");
+  render(node, template(value));
+  console.timeEnd("render");
+}
+
+let value = 0;
+
+const target = document.getElementById("app");
+
+if (target === null) {
+  throw new Error("target missing");
+}
+
+update(target, value);
+
+document.getElementById("increment")?.addEventListener("click", () => {
+  value++;
+  update(target, value);
+});
