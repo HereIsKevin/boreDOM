c76f44bb699392ce8dd85a7cd31af77de052c3b3
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-25 16:09:38 -0700

    Create an optimized VanillaJS example

---
+++ examples/optimized_vanilla/optimized_vanilla.ts
@@ -0,0 +1,50 @@
+function html(
+  strings: TemplateStringsArray,
+  ...values: string[]
+): DocumentFragment {
+  const result: string[] = [strings[0]];
+
+  for (const [index, value] of strings.slice(1).entries()) {
+    result.push(values[index]);
+    result.push(value);
+  }
+
+  return document.createRange().createContextualFragment(result.join(""));
+}
+
+function initialize() {
+  console.time("render");
+
+  const app = document.getElementById("app");
+
+  if (app === null)  {
+    throw new Error("app missing");
+  }
+
+  app.appendChild(
+    html`<div id="display">
+        Value is currently:
+        <!--separator-->0
+      </div>
+      <button id="increment">Click to increment</button>`
+  );
+
+  const display = document.getElementById("display")?.childNodes[2];
+  let value = 0;
+
+  if (typeof display === "undefined") {
+    throw new Error("display missing");
+  }
+
+  document.getElementById("increment")?.addEventListener("click", () => {
+    console.time("render");
+    value++;
+    display.nodeValue = String(value);
+    app.appendChild(html`<div>${String(value - 1)}</div>`)
+    console.timeEnd("render");
+  })
+
+  console.timeEnd("render");
+}
+
+initialize();
