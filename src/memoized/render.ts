a3d049c65be55be564c173ab5ad2e6327c04792b
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-26 09:45:58 -0700

    Begin on optimization of render

--- src/memoized/render.ts
+++ src/memoized/render.ts
@@ -17,26 +17,47 @@ declare global {
   }
 }
 
+function objectZip<K extends string | number, V>(
+  keys: K[],
+  values: V[]
+): Record<K, V> {
+  const result = {} as Record<K, V>;
+
+  for (const [index, key] of keys.entries()) {
+    result[key] = values[index];
+  }
+
+  return result;
+}
+
 function render(target: Element, rawTemplate: RawTemplate): void {
+  // initialize cache if missing, using WeakMap to prevent memory leaks
   if (typeof window.templates === "undefined") {
     window.templates = new WeakMap();
   }
 
+  // attempt to get cache
   const cache = window.templates.get(target);
 
+  // fully cache if cache is missing or if it is the first render
   if (!window.templates.has(target) || typeof cache === "undefined") {
+    // generate a full template with cache, very slow
     const fullTemplate = template(rawTemplate);
 
+    // remove all existing children from target
     while (target.lastChild) {
       target.lastChild.remove();
     }
 
+    // add the document fragment of the template to the target
     target.append(fullTemplate.fragment);
 
+    // filter out the indexes of all the attributes
     const attributes = fullTemplate.attributes.map((x) => x.index);
     const elements = fullTemplate.elements.map((x) => x.index);
     const texts = fullTemplate.texts.map((x) => x.index);
 
+    // cache the template and filtered indexes
     window.templates.set(target, {
       template: fullTemplate,
       attributeIndexes: attributes,
