ae7b4ad206038fcd90b1351d06aab5ae6c154023
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-26 21:08:30 -0700

    Start fixing the faulty diff algorithm

--- src/memoized/diff.ts
+++ src/memoized/diff.ts
@@ -3,16 +3,20 @@ export { diffNodes, eraseNodes };
 import { rawFragment } from "./raw";
 
 function eraseNodes(start: Comment, end: Comment, value: string): void {
+  // iterate through all nodes in between start and end
   while (start.nextSibling !== end) {
     const sibling = start.nextSibling;
 
+    // stop if end is missing
     if (sibling === null) {
       break;
     }
 
+    // remove the node
     sibling.remove();
   }
 
+  // insert the value as a fragment after start
   start.after(rawFragment(value));
 }
 
