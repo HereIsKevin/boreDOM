da48f10b02f47f15d806b6c54cbe3b24378e224a
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-27 18:13:45 -0700

    Fully comment and describe diff system

--- src/memoized/diff.ts
+++ src/memoized/diff.ts
@@ -28,6 +28,7 @@ function diffValues(
 
   for (const [newIndex, newValue] of newValues.entries()) {
     for (const [oldIndex, oldValue] of oldValues.entries()) {
+      // keep the new and old indexes for reference on matches
       if (newValue === oldValue) {
         filtered.push([newIndex, oldIndex]);
       }
@@ -38,16 +39,20 @@ function diffValues(
   let last = [-1, -1];
 
   for (const point of filtered) {
+    // make sure there are no overlaps between existing points
     if (point[0] > last[0] && point[1] > last[1]) {
       keep.push(point);
       last = point;
     }
   }
 
+  // separate the old and new indexes
   const keepOld = keep.map((x) => x[1]);
   const keepNew = keep.map((x) => x[0]);
 
+  // indexes to remove are all the indexes not kept in the old values
   const remove = [...oldValues.keys()].filter((x) => !keepOld.includes(x));
+  // indexes to insert are all the indexes not kept in the new values
   const insert = [...newValues.keys()].filter((x) => !keepNew.includes(x));
 
   return [remove, insert];
@@ -59,42 +64,62 @@ function diffNodes(
   newValue: string[],
   oldValue: string[]
 ): void {
+  // diff original string values instead of child nodes for performance
   const [remove, insert] = diffValues(newValue, oldValue);
 
   let index = 0;
   let current = start.nextSibling;
   const groups: ChildNode[][] = [];
 
+  // iterate through all nodes between start and end
   while (current !== end && current !== null) {
     if (current instanceof Comment && current.nodeValue === "separator") {
+      // begin a new group on a separator
       groups[index] = [current];
       index++;
     } else {
+      // append to newest existing group otherwise
       groups[groups.length - 1].push(current);
     }
 
+    // advance to next sibling
     current = current.nextSibling;
   }
 
+  // iterate through indexes to remove with modifier to account for already
+  // removed nodes, which works as the indexes are sorted in ascending order
+
   for (const [modifier, index] of remove.entries()) {
+    // retrieve group with modifier
     const group = groups[index - modifier];
 
+    // remove all nodes that are part of the group
     for (const node of group) {
       node.remove();
     }
 
+    // remove the group from the groups
     groups.splice(index - modifier, 1);
   }
 
+  // iterate through indexes to insert without modifier, as the indexes will
+  // already be accounted for through the already inserted nodes, which works
+  // due to the indexes being sorted in ascending order
+
   for (const index of insert) {
+    // retrieve the group using index directly
     const group = groups[index];
+    // generate fragment with separator
     const fragment = rawFragment(`<!--separator-->${newValue[index]}`);
 
+    // insert child nodes as a group for padding and reference in the future
     groups.splice(index, 0, [...fragment.childNodes]);
 
     if (typeof group === "undefined") {
+      // insert at the end for undefined indexes
       end.before(fragment);
     } else {
+      // insert after the last node of the group otherwise
       group[group.length - 1].after(fragment);
     }
   }
