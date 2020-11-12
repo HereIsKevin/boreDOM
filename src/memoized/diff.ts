c80b0ba7aeb393619b2e846190ec60915a8db4c8
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-11-11 22:00:53 -0800

    Implement improved diff for DOM nodes

--- src/memoized/diff.ts
+++ src/memoized/diff.ts
@@ -1,21 +1,158 @@
-export { eraseNodes };
+export { diffNodes };
 
 import { rawFragment } from "./raw";
 
-function eraseNodes(start: Comment, end: Comment, value: string): void {
-  // iterate through all nodes in between start and end
-  while (start.nextSibling !== end) {
-    const sibling = start.nextSibling;
+/*
+function includesNode(nodes: Node[], value: Node): boolean {
+  for (const node of nodes) {
+    if (node.isEqualNode(value)) {
+      return true;
+    }
+  }
 
-    // stop if end is missing
-    if (sibling === null) {
+  return false;
+}
+
+function indexOfNode(nodes: Node[], value: Node): number {
+  for (let index = 0; index < nodes.length; index++) {
+    if (nodes[index].isEqualNode(value)) {
+      return index;
+    }
+  }
+
+  return -1;
+}
+
+function diffNodes(start: Comment, end: Comment, value: string): void {
+  console.log("asdf")
+
+  const finalNodes = Array.from(rawFragment(value).childNodes);
+  const cache: Node[] = [];
+
+  let current = start.nextSibling;
+  let index = 0;
+
+  while (true) {
+    if (current === null) {
       break;
     }
 
-    // remove the node
-    sibling.remove();
+    const final = finalNodes[index];
+
+    if (current.isEqualNode(final)) {
+      index++;
+      current = current.nextSibling;
+    } else if (includesNode(cache, final)) {
+      const cacheIndex = indexOfNode(cache, final);
+      const cacheNode = cache.splice(cacheIndex, 1)[0];
+      current.replaceWith(cacheNode);
+
+      cache.push(current);
+      current = cacheNode as ChildNode;
+    } else if (index < finalNodes.length) {
+      current.replaceWith(final.cloneNode(true));
+
+      cache.push(current);
+      current = final;
+    } else if (current.nextSibling !== end) {
+      current.nextSibling?.remove();
+    } else {
+      break;
+    }
+  }
+}
+*/
+
+function includesNode(nodes: Node[], value: Node): boolean {
+  for (const node of nodes) {
+    if (node.isEqualNode(value)) {
+      return true;
+    }
   }
 
-  // insert the value as a fragment after start
-  start.after(rawFragment(value));
+  return false;
+}
+
+function indexOfNode(nodes: Node[], value: Node): number {
+  for (let index = 0; index < nodes.length; index++) {
+    if (nodes[index].isEqualNode(value)) {
+      return index;
+    }
+  }
+
+  return -1;
+}
+
+function collectNodes(start: Comment, end: Comment): Node[] {
+  const nodes: Node[] = [];
+
+  let current = start.nextSibling;
+
+  while (current !== end && current !== null) {
+    nodes.push(current);
+    current = current.nextSibling;
+  }
+
+  return nodes;
+}
+
+function diffNodes(start: Comment, end: Comment, value: string): void {
+  const oldNodes = collectNodes(start, end);
+  const newNodes = Array.from(rawFragment(value).childNodes);
+
+  // console.log(oldNodes, newNodes);
+
+  if (oldNodes.length === 0) {
+    start.after(...newNodes);
+    return;
+  }
+
+  let oldIndex = 0
+  let newIndex = 0
+
+  const cache: Node[] = [];
+
+  for (const newNode of newNodes) {
+    if (oldIndex >= oldNodes.length) {
+      break;
+    }
+
+    const oldNode = oldNodes[oldIndex];
+
+    if (newNode.isEqualNode(oldNode)) {
+      newIndex++;
+      oldIndex++;
+      // console.log("same");
+    } else {
+      newIndex++;
+      (oldNodes[oldIndex] as ChildNode).remove()
+      cache.push(oldNodes.splice(oldIndex, 1)[0]);
+      // console.log("remove")
+    }
+  }
+
+  // console.log(oldNodes)
+
+  let index = 0;
+
+  oldNodes.push(end);
+
+  while (index < newNodes.length) {
+    const oldNode = index < oldNodes.length ? oldNodes[index] : null;
+    const newNode = newNodes[index]
+
+    if (newNode.isEqualNode(oldNode)) {
+      index++;
+    } else if (includesNode(cache, newNode)) {
+      const node = cache.splice(indexOfNode(cache, newNode), 1)[0];
+      (oldNodes[index] as ChildNode).before(node);
+      oldNodes.splice(index, 0, node);
+    } else {
+      const node = newNode.cloneNode(true);
+      (oldNodes[index] as ChildNode).before(node);
+      oldNodes.splice(index, 0, node);
+    }
+  }
+
+  // console.log(oldNodes)
 }
