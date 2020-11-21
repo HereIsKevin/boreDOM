ebff4261513d984d6c9770601ac485ac6136b414
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-11-20 18:57:07 -0800

    Improve diff testing example

--- examples/test_diff/test_diff.ts
+++ examples/test_diff/test_diff.ts
@@ -2,8 +2,16 @@ import { memoized } from "../../dist/esm/index.js";
 
 const { html, render } = memoized;
 
+declare global {
+  interface Window {
+    removeValues: (event: Event) => void;
+  }
+}
+
 const createRows = (count: number) =>
-  [...Array(count + 1000).keys()].slice(count).map((x) => `<div>${x}</div>`);
+  [...Array(count + 1000).keys()]
+    .slice(count)
+    .map((x) => `<button onclick="window.removeValues(event);">${x}</button>`);
 
 const template = (rows: string[]) =>
   html`
@@ -13,11 +21,19 @@ const template = (rows: string[]) =>
       <button id="swap">Swap</button>
       <button id="update">Update</button>
     </div>
-    <div>${rows.join("")}</div>
+    <div class="items">${rows.join("")}</div>
   `;
 
 const add = (target: Element, rows: string[]) => {
   const newRows = [...rows, ...createRows(rows.length)];
+
+  render(target, template(newRows));
+
+  return newRows;
+};
+
+const remove = (target: Element, rows: string[], index: number) => {
+  const newRows = [...rows.slice(0, index), ...rows.slice(index + 1)];
 
   render(target, template(newRows));
 
@@ -49,10 +65,11 @@ const update = (target: Element, rows: string[]) => {
   const newRows = [...rows];
 
   for (let index = 0; index < newRows.length; index += 10) {
-    newRows[index] = `<div>${newRows[index].slice(
-      5,
-      newRows[index].length - 6
-    )} !!!</div>`;
+    newRows[index] = `
+      <button onclick="window.removeValues(event);">
+        ${`${newRows[index].slice(46, newRows[index].length - 9)} !!!`.trim()}
+      </button>
+    `.trim();
   }
 
   render(target, template(newRows));
@@ -69,6 +86,24 @@ const initialize = () => {
   }
 
   render(target, template(rows));
+
+  window.removeValues = (event: Event) => {
+    if (!(event.target instanceof HTMLElement)) {
+      throw new TypeError("event target must be HTMLElement");
+    }
+
+    if (target === null) {
+      throw new Error("target missing");
+    }
+
+    const childNodes = target.children[1].childNodes;
+
+    for (let index = -1; index < childNodes.length - 2; index++) {
+      if (childNodes[index + 1] === event.target) {
+        rows = remove(target, rows, Number(index));
+      }
+    }
+  };
 
   document.getElementById("add")?.addEventListener("click", () => {
     if (target === null) {
