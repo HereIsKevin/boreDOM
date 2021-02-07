b94fa9fe3719e96cd3af050364d6847890fbb2bb
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-02-07 15:56:04 -0800

    Revert "Commit experiments"
    This reverts commit 851f0cd362c4a23be67894895e011eb5c50071bf.

--- src/memoized/render.ts
+++ src/memoized/render.ts
@@ -1,100 +1,130 @@
 export { render };
 
-import { rawFragment, RawTemplate, RawValues } from "./raw";
-import { Template } from "./template";
+import { RawTemplate, RawValues } from "./raw";
+import {
+  TemplateAttribute,
+  TemplateElement,
+  TemplateText,
+  template,
+} from "./template";
+import { compare, diff } from "./diff";
 
-const templates: WeakMap<Node, Template> = new WeakMap();
+interface Cache {
+  values: RawValues;
+  attributes: Record<number, TemplateAttribute>;
+  elements: Record<number, TemplateElement>;
+  texts: Record<number, TemplateText>;
+}
 
-function collapseAttribute(parts: string[], values: RawValues): string {
-  let result = parts[0];
+declare global {
+  interface Window {
+    templates?: WeakMap<Element, Cache>;
+  }
+}
 
-  for (let index = 1; index < parts.length; index++) {
-    const value = values[index - 1];
+function objectZip<K extends string | number, V>(
+  keys: K[],
+  values: V[]
+): Record<K, V> {
+  const result = {} as Record<K, V>;
 
-    if (typeof value !== "string") {
-      throw new TypeError("attribute value must be a string");
-    }
-
-    result += value;
-    result += parts[index];
+  for (const [index, key] of keys.entries()) {
+    // set the corresponding value to the key
+    result[key] = values[index];
   }
 
   return result;
 }
 
-function render(target: Node, rawTemplate: RawTemplate): void {
-  const template = templates.get(target);
+function render(target: Element, rawTemplate: RawTemplate): void {
+  // initialize cache if missing, using WeakMap to prevent memory leaks
+  if (typeof window.templates === "undefined") {
+    window.templates = new WeakMap();
+  }
 
-  if (!templates.has(target) || typeof template === "undefined") {
-    const template = new Template(rawTemplate);
+  // attempt to get cache
+  const cache = window.templates.get(target);
 
+  // fully cache if cache is missing or if it is the first render
+  if (!window.templates.has(target) || typeof cache === "undefined") {
+    // generate a full template with cache, very slow
+    const fullTemplate = template(rawTemplate);
+
+    // remove all existing children from target
     while (target.lastChild) {
       target.lastChild.remove();
     }
 
-    target.appendChild(template.fragment);
-    templates.set(target, template);
+    // add the document fragment of the template to the target
+    target.append(fullTemplate.fragment);
+
+    // filter out the indexes of all the attributes
+    const attributeIndexes = fullTemplate.attributes.map((x) => x.index);
+    // filter out the indexes of all the elements
+    const elementIndexes = fullTemplate.elements.map((x) => x.index);
+    // filter out the indexes of all the texts
+    const textIndexes = fullTemplate.texts.map((x) => x.index);
+
+    // cache the template values and zipped index-based dynamic items
+    window.templates.set(target, {
+      values: fullTemplate.values,
+      attributes: objectZip(attributeIndexes, fullTemplate.attributes),
+      elements: objectZip(elementIndexes, fullTemplate.elements),
+      texts: objectZip(textIndexes, fullTemplate.texts),
+    });
 
     return;
   }
 
-  for (const attribute of template.attributes) {
-    const oldValue = attribute.value;
-    const newValue = collapseAttribute(
-      attribute.parts,
-      attribute.indexes.map((index) => rawTemplate.values[index])
-    );
+  // iterate through each value in the cache
+  for (let index = 0; index < cache.values.length; index++) {
+    const oldValue = cache.values[index];
+    const newValue = rawTemplate.values[index];
 
-    if (oldValue !== newValue) {
-      attribute.element.setAttribute(attribute.name, newValue);
-      attribute.value = newValue;
-    }
-  }
-
-  for (const node of template.nodes) {
-    const oldValue = node.value;
-    const newValue = rawTemplate.values[node.index];
-
-    if (
-      (typeof oldValue === "string" && typeof newValue !== "string") ||
-      (Array.isArray(oldValue) && !Array.isArray(newValue)) ||
-      (oldValue instanceof Template &&
-        typeof newValue !== "string" &&
-        !Array.isArray(newValue))
-    ) {
-      while (node.start.nextSibling !== null && node.start.nextSibling !== node.end) {
-        node.start.nextSibling.remove();
-      }
-
-      if (typeof newValue === "string") {
-        node.start.after(rawFragment(newValue));
-        node.value = newValue
-      } else if (Array.isArray(newValue)) {
-        const result = new DocumentFragment();
-        const templates: Template[] = [];
-    
-        for (const value of newValue) {
-          const template = new Template(value);
-    
-          templates.push(template);
-          result.append(rawFragment("<!--separator-->"));
-          result.append(template.fragment);
-        }
-
-        node.start.after(result);
-        node.value = templates;
-      } else {
-        const template = new Template(newValue);
-
-        node.start.after(template.fragment);
-        node.value = template;
-      }
-
+    // proceed only if the old and new values are different
+    if (oldValue === newValue) {
       continue;
     }
 
-    if (typeof newValue === "string") {
-      
+    if (index in cache.attributes) {
+      // extract element and name of attribute from cache
+      const { element, name } = cache.attributes[index];
+
+      // make sure the new value is not an array
+      if (Array.isArray(newValue)) {
+        throw new TypeError("attribute value cannot be an array");
+      }
+
+      // update the attribute
+      element.setAttribute(name, newValue);
+    } else if (index in cache.texts) {
+      // extract the text node from the cache
+      const { text } = cache.texts[index];
+
+      // make sure the new value is not an array
+      if (Array.isArray(newValue)) {
+        throw new TypeError("text value cannot be an array");
+      }
+
+      // update the text node value
+      text.nodeValue = newValue;
+    } else if (index in cache.elements) {
+      // extract the start and end comments from the cache
+      const { start, end } = cache.elements[index];
+
+      if (Array.isArray(newValue) && Array.isArray(oldValue)) {
+        compare(start, end, oldValue, newValue);
+      } else if (typeof newValue === "string" && typeof oldValue === "string") {
+        // diff and update the old and new values
+        diff(start, end, newValue);
+      } else {
+        throw new TypeError("element value must have consistent types");
+      }
     }
   }
+
+  // update the values in the cache
+  cache.values = rawTemplate.values;
+  // update the cache in the WeakMap
+  window.templates.set(target, cache);
 }
