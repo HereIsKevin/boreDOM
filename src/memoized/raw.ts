c528c510e4f3cc582cf0ab72ab85da37e3f82f86
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-02-07 17:37:20 -0800

    Add documentation to html

--- src/memoized/raw.ts
+++ src/memoized/raw.ts
@@ -18,6 +18,45 @@ function rawFragment(value: string): DocumentFragment {
   return template.content;
 }
 
+/**
+ * Creates a raw template from template literals.
+ *
+ * @param strings Static parts of the template.
+ * @param values Dynamic parts of the template.
+ *
+ * @returns Raw template created from strings and values.
+ *
+ * @example Template for counter
+ *
+ * ```typescript
+ * let value = 0;
+ * const history = [];
+ *
+ * const store = () => history.push(`${value} came before current value`);
+ * const update = () => render(document.body, template(value, history));
+ *
+ * const decrement = () => {
+ *   store();
+ *   value--;
+ *   update();
+ * };
+ *
+ * const increment = () => {
+ *   store();
+ *   value++;
+ *   update();
+ * };
+ *
+ * const template = (value, history) =>
+ *   html`
+ *     <button onclick=${decrement}>-</button>
+ *     <div>${value}</div>
+ *     <button onclick=${increment}>+</button>
+ *     <div>${history}</div>
+ *   `;
+ * ```
+ */
+
 function html(
   strings: TemplateStringsArray,
   ...values: RawValues
