9e480ad1834e84c2bbca5fb477a265f95629d41a
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-02-15 15:56:59 -0800

    Improved render

--- src/memoized/render.ts
+++ src/memoized/render.ts
@@ -1,42 +1,11 @@
 export { Target, render };
 
-import { RawTemplate, RawValues } from "./raw";
-import {
-  TemplateAttribute,
-  TemplateElement,
-  TemplateText,
-  template,
-} from "./template";
-import { compare, diff } from "./diff";
+import { RawTemplate, RawValue } from "./raw";
+import { Template } from "./template";
 
 type Target = Element | ShadowRoot;
 
-interface Cache {
-  values: RawValues;
-  attributes: Record<number, TemplateAttribute>;
-  elements: Record<number, TemplateElement>;
-  texts: Record<number, TemplateText>;
-}
-
-declare global {
-  interface Window {
-    templates?: WeakMap<Target, Cache>;
-  }
-}
-
-function objectZip<K extends string | number, V>(
-  keys: K[],
-  values: V[]
-): Record<K, V> {
-  const result = {} as Record<K, V>;
-
-  for (const [index, key] of keys.entries()) {
-    // set the corresponding value to the key
-    result[key] = values[index];
-  }
-
-  return result;
-}
+const templates = new WeakMap<Target, Template>();
 
 /**
  * Renders a template created with {@link html} to the targeted element.
@@ -58,109 +27,23 @@ function objectZip<K extends string | number, V>(
  */
 
 function render(target: Target, rawTemplate: RawTemplate): void {
-  // initialize cache if missing, using WeakMap to prevent memory leaks
-  if (typeof window.templates === "undefined") {
-    window.templates = new WeakMap();
-  }
+  const cache = templates.get(target);
 
-  // attempt to get cache
-  const cache = window.templates.get(target);
+  if (!templates.has(target) || typeof cache === "undefined") {
+    const fullTemplate = new Template(rawTemplate);
 
-  // fully cache if cache is missing or if it is the first render
-  if (!window.templates.has(target) || typeof cache === "undefined") {
-    // generate a full template with cache, very slow
-    const fullTemplate = template(rawTemplate);
-
-    // remove all existing children from target
     while (target.lastChild) {
       target.lastChild.remove();
     }
 
-    // add the document fragment of the template to the target
     target.append(fullTemplate.fragment);
-
-    // filter out the indexes of all the attributes
-    const attributeIndexes = fullTemplate.attributes.map((x) => x.index);
-    // filter out the indexes of all the elements
-    const elementIndexes = fullTemplate.elements.map((x) => x.index);
-    // filter out the indexes of all the texts
-    const textIndexes = fullTemplate.texts.map((x) => x.index);
-
-    // cache the template values and zipped index-based dynamic items
-    window.templates.set(target, {
-      values: fullTemplate.values,
-      attributes: objectZip(attributeIndexes, fullTemplate.attributes),
-      elements: objectZip(elementIndexes, fullTemplate.elements),
-      texts: objectZip(textIndexes, fullTemplate.texts),
-    });
+    templates.set(target, fullTemplate);
 
     return;
   }
 
-  // iterate through each value in the cache
-  for (let index = 0; index < cache.values.length; index++) {
-    const oldValue = cache.values[index];
-    const newValue = rawTemplate.values[index];
+  cache.update(rawTemplate.values);
 
-    // proceed only if the old and new values are different
-    if (oldValue === newValue) {
-      continue;
-    }
-
-    if (index in cache.attributes) {
-      // extract element and name of attribute from cache
-      const { element, name } = cache.attributes[index];
-
-      if (
-        /^on[a-z]+$/.test(name) &&
-        typeof oldValue !== "string" &&
-        !Array.isArray(oldValue)
-      ) {
-        // remove old event handlers
-        element.removeEventListener(name, oldValue);
-      }
-
-      if (Array.isArray(newValue)) {
-        // make sure the new value is not an array
-        throw new TypeError("attribute value cannot be an array");
-      } else if (typeof newValue === "string") {
-        // update the attribute
-        element.setAttribute(name, newValue);
-      } else if (/^on[a-z]+$/.test(name)) {
-        // add new event handlers for "on" attributes
-        element.addEventListener(name, newValue);
-      } else {
-        // regular attributes cannot take event handlers
-        throw new TypeError("non-handler attributes cannot be handlers");
-      }
-    } else if (index in cache.texts) {
-      // extract the text node from the cache
-      const { text } = cache.texts[index];
-
-      // make sure the new value is a string
-      if (typeof newValue !== "string") {
-        throw new TypeError("text value must be a string");
-      }
-
-      // update the text node value
-      text.nodeValue = newValue;
-    } else if (index in cache.elements) {
-      // extract the start and end comments from the cache
-      const { start, end } = cache.elements[index];
-
-      if (Array.isArray(newValue) && Array.isArray(oldValue)) {
-        compare(start, end, oldValue, newValue);
-      } else if (typeof newValue === "string" && typeof oldValue === "string") {
-        // diff and update the old and new values
-        diff(start, end, newValue);
-      } else {
-        throw new TypeError("element value must have consistent types");
-      }
-    }
-  }
-
-  // update the values in the cache
-  cache.values = rawTemplate.values;
-  // update the cache in the WeakMap
-  window.templates.set(target, cache);
+  cache.raw.values = rawTemplate.values;
+  templates.set(target, cache);
 }
